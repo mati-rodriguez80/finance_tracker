0b478f148a7acf1d40b51c10d5b5a6e05d0f3a35
mati-rodriguez80 <95318985+mati-rodriguez80@users.noreply.github.com>
2022-07-15 14:40:36 -0300

    Add friends list and search friends form with basic response to start with

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
 end
